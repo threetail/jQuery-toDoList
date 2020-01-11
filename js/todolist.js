93d63434befeb6d82b9bcfef977ec16268c6d736
threetail <1160047418@qq.com>
2020-01-11 22:31:44 +0800

    做了添加和渲染接下来是删除操作

--- js/todolist.js
+++ js/todolist.js
@@ -1,3 +1,36 @@
-$(function(){
-    
+$(function () {
+    load()
+    $('#title').on("keyup", function (e) {
+        if (e.keyCode === 13) {
+            var data = getData();
+            data.push({todo:$(this).val(),status:false})
+            setData(data)
+            load()
+        }
+    })
+    // 存储数据
+    function setData(data) {
+        localStorage.setItem('todo', JSON.stringify(data))
+    }
+
+    //读取数据
+    function getData() {
+        var data = localStorage.getItem('todo');
+        if (data === null) {
+            return []
+        } else {
+            return JSON.parse(data)
+        }
+    }
+
+    //渲染加载数据
+    function load(){
+        var data = getData();
+        $('ol,ul').empty()
+        $.each(data,function(i,n){
+            $('ol').prepend('<li><input type="checkbox"><a></a>'+n.todo+'</li>')        
+        })
+        
+    }
+
 })
